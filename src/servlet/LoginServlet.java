4840cd4c826df4c8d174719fe0454b1975b5c346
iyo-marina <iyo-marina@KMS2013-114.kms.local>
2018-05-29 17:01:26 +0900

    LoginModel、LoginServletコメント追加-処理内容コメントアウト

--- src/servlet/LoginServlet.java
+++ src/servlet/LoginServlet.java
@@ -49,17 +49,25 @@ public class LoginServlet extends HttpServlet {
 			bean = model.authentication(bean);
 		} catch (Exception e) {
 			e.printStackTrace();
+			System.out.println("ログインサーブレット、認証処理キャッチ");
 		}
-		// 取得に成功した場合セッション情報をセット
-		if ("".equals(bean.getErrorMessage())) {
+		// 登録済み会員情報の取得に成功した場合(ログイン可の場合)セッション情報をセット
+		if ("".equals(bean.getErrorMessage())) {			// ログイン成功の場合、エラーメッセは空文字
+			// ますはSessionBeanに情報をセット
 			SessionBean sessionBean = new SessionBean();
 			sessionBean.setUserName(bean.getUserName());
 			sessionBean.setUserNo(bean.getUserNo());
+			// セッションに、SessionBeanをセット
 			HttpSession session = req.getSession();
 			session.setAttribute("session", sessionBean);
 
-			// 行き先を次の画面に
+			// 行き先を次の画面に（サーブレットのURLを指定）
 			direction = "/main";
+
+		// 登録済み会員情報の取得に失敗した場合(ログイン不可の場合)、ログイン画面に遷移しエラーメッセを表示
+		} else {
+
+			System.out.println("ログインサーブレットログイン不可のelseにいます");
 		}
 		req.getRequestDispatcher(direction).forward(req, res);
 	}
