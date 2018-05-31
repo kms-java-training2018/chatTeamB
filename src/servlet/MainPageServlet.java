ac39abb9d80505aec068752cf0df4b85ab81ab04
mitsuno-shinki <mitsuno-shinki@KMS2013-61.kms.local>
2018-05-31 10:00:11 +0900

    三野

--- src/servlet/MainPageServlet.java
+++ src/servlet/MainPageServlet.java
@@ -52,9 +52,6 @@ public class MainPageServlet extends HttpServlet {
 
 	}
 
-	// Beanを使うための初期化
-			LoginBean bean = new LoginBean();
-			String userNo = bean.getUserNo();
-			String userName = bean.getUserName();
+
 
 }
