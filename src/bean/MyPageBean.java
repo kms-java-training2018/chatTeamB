cd13a372a037fd947b45716bfbf331108e392147
inada-yuji <inada-yuji@KMS2013-49.kms.local>
2018-05-31 16:38:58 +0900

    他ユーザープロフィール確認servlet,model///プロフィール編集画面servlet,model

---
+++ src/bean/MyPageBean.java
@@ -0,0 +1,24 @@
+package bean;
+
+public class MyPageBean {
+	private String userName;
+
+	private String myPageText;
+
+	public String getMyPageText() {
+		return myPageText;
+	}
+
+	public void setMyPageText(String myPageText) {
+		this.myPageText = myPageText;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+}
