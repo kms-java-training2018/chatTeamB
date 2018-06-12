da2a3a2e827a90335eaa8be8410d4f10b7881c20
iyo-marina <iyo-marina@KMS2013-114.kms.local>
2018-06-12 14:36:16 +0900

    ログアウト処理をsession.invalidate();に変更

--- src/servlet/LogoutServlet.java
+++ src/servlet/LogoutServlet.java
@@ -21,7 +21,7 @@ public class LogoutServlet extends HttpServlet {
 
 		HttpSession session = req.getSession();
 		session = req.getSession(false);
-		session = null;
+		session.invalidate();
 		req.getRequestDispatcher("/WEB-INF/jsp/logout.jsp").forward(req, res);
 
 	}
